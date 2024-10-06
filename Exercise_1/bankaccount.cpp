72a2752f6da4984ca88d33c74808a2de62743809
jladams42 <jladams42@tntech.edu>
2024-10-06 13:47:42 -0500

    Adding new structure

--- Exercise_1/bankaccount.cpp
+++ Exercise_1/bankaccount.cpp
@@ -6,30 +6,41 @@
 using namespace std;
 
 class BankAccount {
+    // Private class attributes to be accessed within the class functions.
     private:
         string accountHolderName;
         int accountNumber;
         double balance;
         
     public:
+        // Constructor for the BankAccount class. 
         BankAccount(string accountHolderName, int accountNumber, double balance) {
             this->accountHolderName = accountHolderName;
             this->accountNumber = accountNumber;
             this->balance = balance;
-        };
+        }
+        // Void function to display the account name, number, and balance.
         void displayAccountInfo() {
             cout << "Account Holder: "; cout << accountHolderName << endl;
             cout << "Account Number: "; cout << accountNumber << endl;
             cout << "Account Balance: $"; cout << balance << endl; 
             cout << "--------------------------" << endl;
-        };
-        int deposit(int x){
-            this->balance = balance + x;
-            return balance;
         }
-        int withdraw(int x){
-            this->balance = balance - x;
-            return balance; 
+        // Deposit function.
+        void deposit(int x){
+            cout << "After depositing $" << x << endl;
+            this->balance = balance + x;
+        }
+        // Withdraw function with validation.
+        void withdraw(int x){
+            if (balance - x < 0){
+                cout << "Insufficient balance!\n";
+                cout << "After attempting to withdraw $" << x << endl;
+            } else {
+                cout << "After withdrawing $" << x << endl;
+                this->balance = balance - x;
+            }
+            
         }
 };
 
@@ -41,9 +52,12 @@ int main() {
     myAccount.displayAccountInfo();
     // Deposit money
     myAccount.deposit(150.00);
-    cout << "After depositing $150:" << endl; myAccount.displayAccountInfo();
+    myAccount.displayAccountInfo();
     // Withdraw money
     myAccount.withdraw(100.00);
-    cout << "After withdrawing $100:" << endl; myAccount.displayAccountInfo();
+    myAccount.displayAccountInfo();
+    // Attempt to withrdaw more than the balance.
+    myAccount.withdraw(600.00);
+    myAccount.displayAccountInfo();
     return 0; 
 }
--- Lab_3/Driver.cpp
+++ Lab_3/Driver.cpp
@@ -5,7 +5,10 @@
 using namespace std;
 
 //LOOK!!  ENTER YOUR FUNCTION PROTOTYPES HERE
-
+void insertionSort(Song mySongArray[], int numSongs);
+void reverseBubbleSort(Song mySongArray[], int numSongs);
+void quickSort(Song mySongArray[], int left, int right);
+int partition(Song mySongArray[], int left, int right);
 
 int main()
 {
@@ -30,7 +33,7 @@ int main()
 		exit(1);
 	}
 	
-	while(getline(inFile, temp) && numSongs != 150000)
+	while(getline(inFile, temp, '\n') && numSongs != 150000)
 	{
 		mySongArray[numSongs].setTitle(temp);
 		getline(inFile, temp);
@@ -42,13 +45,11 @@ int main()
 	}
 	cout << "\n\nYou have created " << numSongs << " Song objects.\n\n";
 	
-	
-	
 	//sort the songs using insertion sort and print them out to the text file sortFileInsertion.txt
 	start = getTime(); //Starts timer.   
 	
 	//LOOK!!!!  CALL THE INSERTION SORT ALGORITHM HERE
-	
+	insertionSort(mySongArray, numSongs);
 	
 	end = getTime(); //Ends timer.
 	outFile.open("sortFileInsertion.txt");
@@ -58,14 +59,13 @@ int main()
 		outFile << mySongArray[x];
 	}
 	outFile.close();
-	
-	
+
 	
 	//sort the songs in reverse order using bubble sort & print them out to the text file sortFileReverseBubble.txt
 	start = getTime(); //Starts timer. 
 	
 	//LOOK!!!!  CALL THE REVERSE BUBBLE SORT ALGORITHM HERE
-	
+	reverseBubbleSort(mySongArray, numSongs);
 	
 	end = getTime(); //Ends timer.
 	outFile.open("sortFileReverseBubble.txt");
@@ -76,13 +76,11 @@ int main()
 	}
 	outFile.close();
 	
-	
-	
 	//sort the songs with quick sort & print them out to sortFileQuick.txt
 	start = getTime(); //Starts timer. 
 	
 	//LOOK!!!!  CALL THE QUICKSORT ALGORITHM HERE
-	
+	quickSort(mySongArray, 0, numSongs - 1);
 	
 	end = getTime(); //Ends timer.
 	cout << "\nQuicksort: " << totalTime(start, end) << " seconds\n\n";
@@ -99,16 +97,60 @@ int main()
 }
 
 //LOOK!  WRITE YOUR INSERTION SORT FUNCTION HERE
+void insertionSort(Song mySongArray[], int numSongs){
+	for (int i = 1; i < numSongs; ++i){
+		Song key = mySongArray[i];
+		int j = i - 1;
 
-
+		while (j >= 0 && mySongArray[j].getTitle() > key.getTitle()){
+			mySongArray[j + 1] = mySongArray[j];
+			--j;
+		}
+		mySongArray[j + 1] = key;
+	}
+}
 
 //LOOK!  WRITE YOUR REVERSE BUBBLE SORT FUNCTION HERE
-
+void reverseBubbleSort(Song mySongArray[], int numSongs){
+	for (int i = 0; i < numSongs - 1; ++i){ 
+		for (int j = 0; j < numSongs - i - 1; ++j){
+			if (mySongArray[j].getTitle() < mySongArray[j + 1].getTitle()){
+				Song temp = mySongArray[j];
+				mySongArray[j] = mySongArray[j + 1];
+				mySongArray[j + 1] = temp;
+			}
+		}
+	}
+}
 
 
 //LOOK!  WRITE YOUR RECURSIVE QUICK SORT FUNCTION HERE
-
+void quickSort(Song *mySongArray, int left, int right) {
+	if (left < right) {
+		int pivotIndex = partition(mySongArray, left, right);
+		quickSort(mySongArray, left, pivotIndex - 1);
+		quickSort(mySongArray, pivotIndex + 1, right); 
+	}
+}
 
 
 //LOOK!  WRITE YOUR PARTITION FUNCTION HERE
+int partition(Song *mySongArray, int left, int right){
+	string pivot = mySongArray[right].getTitle();
+	int i = left - 1;
 
+	for (int j = left; j < right; ++j){
+		if (mySongArray[j].getTitle() < pivot){
+			i++;
+			Song temp = mySongArray[i];
+			mySongArray[i] = mySongArray[j];
+			mySongArray[j] = temp;
+		}
+	}
+
+	Song temp = mySongArray[i + 1];
+	mySongArray[i + 1] = mySongArray[right];
+	mySongArray[right] = temp;
+
+	return i + 1;
+}
